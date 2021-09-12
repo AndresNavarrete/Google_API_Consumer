b5d2809ea6a092d3d85d5e2237ff60af2e2e30e0
Andrés Navarrete <ainavarrete@uc.cl>
2021-09-11 21:55:22 -0500

    Modify main

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from Consumer import Consumer
 from DataProcessor import DataProcessor
 from InputManager import InputManager
+from time import time
 
-consumer = Consumer()
-inputManager = InputManager()
-dataProcessor = DataProcessor(consumer)
+def main():
+    consumer = Consumer()
+    inputManager = InputManager()
+    dataProcessor = DataProcessor(consumer)
 
-dataProcessor.readInput(inputManager)
-dataProcessor.proccesData()
-dataProcessor.exportExcel()
+    dataProcessor.readInput(inputManager)
+    dataProcessor.proccesData()
+    dataProcessor.exportExcel()
+
+if __name__ == "__main__":
+    initialTime = time.time()
+
+    main()
+
+    finalTime = time.time()
+    secondsElapsed = round(finalTime - initialTime, 2)
+    print("Time elapsed: {} seconds ".format(secondsElapsed))
